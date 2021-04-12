15bc811b9f105c092151f3aab3564ec0ccff11c4
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2021-04-12 13:55:40 +0900

    add

--- VideoSample/VideoSample/ViewController.swift
+++ VideoSample/VideoSample/ViewController.swift
@@ -10,6 +10,12 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    // ここを変更して動画の構成を変更する
+    private let componentType = ".mp4"
+    private let sessionPreset: AVCaptureSession.Preset = .hd1280x720
+    private let codecType: AVVideoCodecType = AVVideoCodecType.hevc
+    private let fpsRate: Int32 = 30
+    
     // MARK: Property
     @IBOutlet weak var recordBtn: UIButton!
     @IBOutlet weak var cameraView: UIView!
@@ -61,17 +67,25 @@ class ViewController: UIViewController {
             }
         }
         
-        let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio)
-        
         // video input setting
         let videoInput = try! AVCaptureDeviceInput(device: innerCamera!)
         captureSession.addInput(videoInput)
         
+        
+        // 音声を入れる場合は、コメントアウトを外す。
         // audio input setting
-        let audioInput = try! AVCaptureDeviceInput(device: audioDevice!)
-        captureSession.addInput(audioInput)
+//        let audioDevice = AVCaptureDevice.default(for: AVMediaType.audio)
+//        let audioInput = try! AVCaptureDeviceInput(device: audioDevice!)
+//        captureSession.addInput(audioInput)
         
         captureSession.addOutput(fileOutput)
+        
+        let connection = fileOutput.connection(with: .video)
+        // コーデックタイプの変更
+        fileOutput.setOutputSettings([AVVideoCodecKey: codecType], for: connection!)
+        
+        let videoDataOutputConnection = fileOutput.connection(with: .video)
+        fileOutput.setRecordsVideoOrientationAndMirroringChangesAsMetadataTrack(true, for: videoDataOutputConnection!)
         
         captureSession.startRunning()
         
@@ -96,8 +110,16 @@ class ViewController: UIViewController {
     func setupImageQuality() {
         captureSession.beginConfiguration()
         
-        // .highを設定すると、16:9の画角になる
-        captureSession.sessionPreset = .high
+        /**
+         * hd1920x1080: 1080p
+         * hd1280x720: 720p
+         * .high: 16:9 high-quality
+         */
+        if captureSession.canSetSessionPreset(sessionPreset) {
+            captureSession.sessionPreset = sessionPreset
+        } else {
+            captureSession.sessionPreset = .hd1280x720
+        }
         
         captureSession.commitConfiguration()
     }
@@ -130,16 +152,18 @@ class ViewController: UIViewController {
     
     // MARK: IBAction
     @IBAction func onClickRecordBtn(_ sender: Any) {
+        
         if fileOutput.isRecording {
-            // stop recording
+            
             fileOutput.stopRecording()
             
             self.recordBtn.backgroundColor = .gray
             self.recordBtn.setTitle("Record", for: .normal)
+            
         } else {
-            // start recording
+        
             let tempDirectory = URL(fileURLWithPath: NSTemporaryDirectory())
-            let fileURL = tempDirectory.appendingPathComponent("mytemp1.mov")
+            let fileURL = tempDirectory.appendingPathComponent("mytemp1" + componentType)
             
             fileOutput.startRecording(to: fileURL, recordingDelegate: self)
             
@@ -155,40 +179,42 @@ extension ViewController: AVCaptureFileOutputRecordingDelegate {
         
         // どこのディレクトリに保存するか
         let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        // 保存するファイル名
         let destinationURL = documentDirectory.appendingPathComponent("hogehoge.mp4")
-        
         // 保存したいファイルタイプ
         let fileType = AVFileType.mp4
-        
+
+        // export処理
         exportMovie(sourceURL: outputFileURL, destinationURL: destinationURL, fileType: fileType)
+
         // show alert
         let alert = UIAlertController(title: "Recorded!", message: outputFileURL.absoluteString, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
         alert.addAction(okAction)
-        
+
         self.present(alert, animated: true, completion: nil)
     }
     
     func exportMovie(sourceURL: URL, destinationURL: URL, fileType: AVFileType) {
         
         let avAsset = AVAsset(url: sourceURL)
-        
-        // videoとaudioのトラックをそれぞれ取得
-        let videoTrack = avAsset.tracks(withMediaType: .video)[0]
-        let audioTracks = avAsset.tracks(withMediaType: .audio)
-        let audioTrack = audioTracks.count > 0 ? audioTracks[0] : nil
-        
         let mainComposition = AVMutableComposition()
         
-        // videoとaudioのコンポジショントラックをそれぞれ作成
+        // video トラックの取得
+        let videoTrack = avAsset.tracks(withMediaType: .video)[0]
+        // videoのコンポジショントラックを作成
         let compositionVideoTrack = mainComposition.addMutableTrack(withMediaType: .video, preferredTrackID: kCMPersistentTrackID_Invalid)!
-        let compositionAudioTrack = audioTrack != nil
-            ? mainComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)!
-            : nil
-        
         // コンポジションの設定
         try! compositionVideoTrack.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: avAsset.duration), of: videoTrack, at: CMTime.zero)
-        try! compositionAudioTrack?.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: avAsset.duration), of: audioTrack!, at: CMTime.zero)
+        
+        // audioを使用する場合はコメントアウトを外す
+//        let audioTracks = avAsset.tracks(withMediaType: .audio)
+//        let audioTrack = audioTracks.count > 0 ? audioTracks[0] : nil
+//        let compositionAudioTrack = audioTrack != nil
+//            ? mainComposition.addMutableTrack(withMediaType: .audio, preferredTrackID: kCMPersistentTrackID_Invalid)!
+//            : nil
+//        try! compositionAudioTrack?.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: avAsset.duration), of: audioTrack!, at: CMTime.zero)
+        
         
         // エクスポートするためのセッションを作成
         let assetExxport = AVAssetExportSession.init(asset: mainComposition, presetName: AVAssetExportPresetMediumQuality)
@@ -207,6 +233,84 @@ extension ViewController: AVCaptureFileOutputRecordingDelegate {
         // エクスポートする
         assetExxport?.exportAsynchronously(completionHandler: {
             // エクスポート完了後に実行する処理
+            // TODO: APIリクエスト等行う？
+            self.compression(beforeMovieUrl: destinationURL)
+        })
+    }
+    
+    func compression(beforeMovieUrl: URL) {
+        let tempDirectory = URL(fileURLWithPath: NSTemporaryDirectory())
+        let videoURL: URL = tempDirectory.appendingPathComponent("\(Date())tmpMovieSample.mov")
+        //コンポジション作成
+        let mixComposition : AVMutableComposition = AVMutableComposition()
+        //アセットの作成
+        //動画のアセットとトラックを作成
+        let videoAsset: AVURLAsset = AVURLAsset(url: beforeMovieUrl, options:nil)
+        // 元の動画ファイルから、動画（音声なし）トラックの取得
+        let videoTrack: AVAssetTrack = (videoAsset.tracks(withMediaType: AVMediaType.video))[0]
+        // ベースとなる動画のコンポジション作成
+        let compositionVideoTrack: AVMutableCompositionTrack! = mixComposition.addMutableTrack(withMediaType: AVMediaType.video, preferredTrackID: kCMPersistentTrackID_Invalid)
+        // 動画の長さ設定
+        try! compositionVideoTrack.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: videoAsset.duration), of: videoTrack, at: CMTime.zero)
+        // 音声付きの動画か判定
+        if videoAsset.tracks(withMediaType: AVMediaType.audio).count > 0 {
+            // 音声付き動画の場合、音声を取得し設定する
+            // 元の動画ファイルから、オーディオトラックの取得
+            let audioTrack: AVAssetTrack = (videoAsset.tracks(withMediaType: AVMediaType.audio))[0]
+            // ベースとなる音声のコンポジション作成
+            let compositionAudioTrack: AVMutableCompositionTrack! = mixComposition.addMutableTrack(withMediaType: AVMediaType.audio, preferredTrackID: kCMPersistentTrackID_Invalid)
+            // 音声の長さ設定
+            try! compositionAudioTrack.insertTimeRange(CMTimeRangeMake(start: CMTime.zero, duration: videoAsset.duration), of: audioTrack, at: CMTime.zero)
+        }
+        // 回転方向の設定
+        compositionVideoTrack.preferredTransform = videoTrack.preferredTransform
+        // 動画のサイズを取得
+        var videoSize: CGSize = videoTrack.naturalSize
+        // 動画が縦の場合は「true」で、横の場合には「false」が入る
+        var isPortrait: Bool = false
+        // ビデオを縦横方向
+        let transform: CGAffineTransform = videoTrack.preferredTransform
+        // 動画が縦かどうかを判定する
+        if (transform.a == 0 && transform.d == 0 && (transform.b == 1.0 || transform.b == -1.0) && (transform.c == 1.0 || transform.c == -1.0)) {
+            // 動画が縦の場合
+            isPortrait = true
+            // 「videoSize」に設定している「高さ」と「幅」を反転させる（デフォルトが横向きのサイズの為）
+            videoSize = CGSize(width: videoSize.height, height: videoSize.width)
+        }
+        // 合成用コンポジション作成
+        let videoComp: AVMutableVideoComposition = AVMutableVideoComposition()
+        videoComp.renderSize = videoSize
+        // フレームレートの設定
+        // TODO: ここの値を調整する事で、動画のフレームレートを落として、容量の削減につながる
+        videoComp.frameDuration = CMTimeMake(value: 1, timescale: fpsRate)
+        // インストラクションを合成用コンポジションに設定
+        let instruction: AVMutableVideoCompositionInstruction = AVMutableVideoCompositionInstruction()
+        instruction.timeRange = CMTimeRangeMake(start: CMTime.zero, duration: videoAsset.duration)
+        let layerInstruction: AVMutableVideoCompositionLayerInstruction = AVMutableVideoCompositionLayerInstruction.init(assetTrack: compositionVideoTrack)
+        instruction.layerInstructions = [layerInstruction]
+        videoComp.instructions = [instruction]
+        if isPortrait {
+            // 動画が縦の場合
+            // 動画を90％回す（「AVAssetExportSession」を使用する場合には、縦の場合でも横向きで表示される為、90％回す必要がある）
+            let FirstAssetScaleFactor: CGAffineTransform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            layerInstruction.setTransform(videoTrack.preferredTransform.concatenating(FirstAssetScaleFactor), at: CMTime.zero)
+        }
+        // 動画のコンポジションをベースにAVAssetExportを生成
+        let _assetExport = AVAssetExportSession.init(asset: mixComposition, presetName: AVAssetExportPresetHighestQuality)
+        // 合成用コンポジションを設定
+        _assetExport?.videoComposition = videoComp
+        let exportUrl: URL = videoURL
+        _assetExport?.outputFileType = AVFileType.mp4
+        _assetExport?.outputURL = exportUrl
+        _assetExport?.shouldOptimizeForNetworkUse = true
+        // エクスポート実行
+        _assetExport?.exportAsynchronously(completionHandler: {() -> Void in
+            if _assetExport?.status == AVAssetExportSession.Status.failed {
+                // 失敗した場合
+            }
+            if _assetExport?.status == AVAssetExportSession.Status.completed {
+                // 成功した場合
+            }
         })
     }
 }
